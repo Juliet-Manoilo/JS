7f1c7649686fbae406101b8abf021b5c35e11f84
juliett manoilo <mnl.juliett@gmail.com>
2020-11-02 02:20:55 +0200

    :construction: adding some tasks

--- dist/js.e31bb0bc.js
+++ dist/js.e31bb0bc.js
@@ -118,31 +118,59 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"index.js":[function(require,module,exports) {
+/* Запросите у пользователя его имя и выведите в ответ: «Привет, его имя!»  */
 var userName = prompt('What is your name?');
 alert("Hello, ".concat(userName, "!"));
+/* Запросите у пользователя год его рождения, посчитайте, сколько ему лет и выведите результат. 
+Текущий год укажите в коде как константу. */
+
 var currentYear = 2020;
 var userYearBirth = Number(prompt('Year of your birth?'));
-alert(currentYear - userYearBirth);
+var userAge = currentYear - userYearBirth;
+alert("Your age is " + userAge);
+/* Запросите у пользователя длину стороны квадрата и выведите периметр такого квадрата.  */
+
 var perimeter = 4;
 var userLength = Number(prompt('Length of your square side?'));
-alert(userLength * perimeter);
+var userPerimeter = userLength * perimeter;
+alert("Perimeter of your square is " + userPerimeter);
+/* Запросите у пользователя радиус окружности и выведите площадь такой окружности.  */
+
 var pi = 3.14;
 var userRadius = Number(prompt('Put the radius'));
-alert(userRadius * 2 * pi);
+var circleArea = userRadius * 2 * pi;
+alert("Area of your circle is " + circleArea);
+/* Запросите у пользователя расстояние в км между двумя городами и за сколько часов он хочет добраться. 
+Посчитайте скорость, с которой необходимо двигаться, чтобы успеть вовремя.  */
+
 var userDistance = Number(prompt('Put the distance between A and B in km'));
-var userSpeed = Number(prompt('How many hours do you want to spent on this travel?'));
-alert(userDistance / userSpeed);
+var userTime = Number(prompt('How many hours do you want to spent on this travel?'));
+var userSpeed = userDistance / userTime;
+alert("You should move with a speed: " + userSpeed + " km/hour");
+/* Реализуйте конвертор валют. Пользователь вводит доллары, программа переводит в евро. Курс валюты храните в константе.  */
+
 var currentExchange = 0.85;
 var userUsdQuantity = Number(prompt('How much money do you want to exchange USD to Euro?'));
-alert(currentExchange * userUsdQuantity);
+var userEuroAmount = currentExchange * userUsdQuantity;
+alert("You will have " + userEuroAmount + " EURO");
+/* Пользователь указывает объем флешки в Гб. Программа должна посчитать, сколько файлов размером в 820 Мб помещается на флешку.  */
+
 var gigabyte = 1024;
 var file = 820;
 var userCard = Number(prompt('How many gigabytes contain your card?'));
-alert(userCard * gigabyte / file);
+var userFilesQuantity = userCard * gigabyte / file;
+alert("Your card storage can contain " + userFilesQuantity + " files per 820 Mb");
+/* Пользователь вводит сумму денег в кошельке и цену одной шоколадки. 
+Программа выводит, сколько шоколадок может купить пользователь, и сколько сдачи у него останется.  */
+
 var userMoney = Number(prompt('How much money do you want to spent on chokolate bars?'));
 var barPrice = Number(prompt('How much money does chokolate bar costs?'));
-alert(userMoney / barPrice);
-alert(userMoney % barPrice);
+var userChocolate = userMoney / barPrice;
+var oddMoney = userMoney % barPrice;
+alert("You can buy " + userChocolate + " chocolate bars and your odd money is " + oddMoney);
+/* Запросите у пользователя трехзначное число и выведите его задом наперед. 
+Для решения задачи вам понадобится оператор % (остаток от деления).  */
+
 var userNumber = Number(prompt('Put any number'));
 var x = userNumber;
 var revers = 0;
@@ -153,13 +181,98 @@ while (x > 0) {
   x = parseInt(x / 10);
 }
 
-alert(revers);
+var numberContrary = revers;
+alert("Your number on the contrary is " + numberContrary);
+/* Пользователь вводит сумму вклада в банк на 2 месяца, с процентной ставкой депозита 5% годовых. 
+Вывести сумму начисленных процентов. */
+
 var userCash = Number(prompt('How much money do you want to put on deposit?'));
 var bankYearPercent = 5;
 var monthsPerYear = 12;
 var twoMonthPercent = bankYearPercent / monthsPerYear * 2;
 var userSummary = userCash + twoMonthPercent;
-alert(userCash * twoMonthPercent / 100);
+var userEarning = userCash * twoMonthPercent / 100;
+alert("You will earn " + userEarning + " for 2 months");
+/* LESSON JS-2: Типи даних та оператори
+Запросить у пользователя его возраст и определить, кем он является: 
+ребенком (0–2), подростком (12–18), взрослым (18_60) или пенсионером (60– ...). */
+
+var userAge2 = Number(prompt('How old are you?'));
+
+switch (true) {
+  case userAge2 < 2:
+    alert('You are a baby');
+    break;
+
+  case userAge2 > 2 && userAge2 < 12:
+    alert('You are a children');
+    break;
+
+  case userAge2 > 12 && userAge2 < 18:
+    alert('You are a teenager');
+    break;
+
+  case userAge2 > 18 && userAge2 < 60:
+    alert('You are adult');
+    break;
+
+  case userAge2 > 60:
+    alert('You are old');
+    break;
+
+  default:
+    break;
+}
+/* Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д). */
+
+
+var userSymbol = Number(prompt('Put any number from 0 to 9'));
+
+switch (userSymbol) {
+  case 0:
+    alert('Your symbol is )');
+    break;
+
+  case 1:
+    alert('Your symbol is !');
+    break;
+
+  case 2:
+    alert('Your symbol is @');
+    break;
+
+  case 3:
+    alert('Your symbol is #');
+    break;
+
+  case 4:
+    alert('Your symbol is $');
+    break;
+
+  case 5:
+    alert('Your symbol is %');
+    break;
+
+  case 6:
+    alert('Your symbol is ^');
+    break;
+
+  case 7:
+    alert('Your symbol is &');
+    break;
+
+  case 8:
+    alert('Your symbol is *');
+    break;
+
+  case 9:
+    alert('Your symbol is (');
+    break;
+
+  default:
+    break;
+}
+/* Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры. */
 },{}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -188,7 +301,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56661" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51702" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- index.js
+++ index.js
@@ -1,35 +1,65 @@
+
+/* Запросите у пользователя его имя и выведите в ответ: «Привет, его имя!»  */
+
 const userName = prompt('What is your name?')
-alert(`Hello, ${userName}!`)
+alert(`Hello, ${userName}!`);
+
+/* Запросите у пользователя год его рождения, посчитайте, сколько ему лет и выведите результат. 
+Текущий год укажите в коде как константу. */
 
 const currentYear = 2020;
 const userYearBirth = Number(prompt('Year of your birth?'))
-alert(currentYear - userYearBirth);
+const userAge = currentYear - userYearBirth
+ alert(`Your age is ` + userAge);
+
+/* Запросите у пользователя длину стороны квадрата и выведите периметр такого квадрата.  */
 
 const perimeter = 4;
 const userLength = Number(prompt('Length of your square side?'))
-alert(userLength * perimeter);
+const userPerimeter = userLength * perimeter
+alert (`Perimeter of your square is ` + userPerimeter);
+
+/* Запросите у пользователя радиус окружности и выведите площадь такой окружности.  */
 
 const pi = 3.14;
 const userRadius = Number(prompt('Put the radius'))
-alert(userRadius *2 *pi);
+const circleArea = userRadius *2 *pi
+alert (`Area of your circle is ` + circleArea);
+
+/* Запросите у пользователя расстояние в км между двумя городами и за сколько часов он хочет добраться. 
+Посчитайте скорость, с которой необходимо двигаться, чтобы успеть вовремя.  */
 
 const userDistance = Number(prompt('Put the distance between A and B in km'))
-const userSpeed = Number(prompt('How many hours do you want to spent on this travel?'))
-alert(userDistance / userSpeed);
+const userTime = Number(prompt('How many hours do you want to spent on this travel?'))
+const userSpeed = userDistance / userTime
+alert (`You should move with a speed: ` + userSpeed + ` km/hour`)
+
+/* Реализуйте конвертор валют. Пользователь вводит доллары, программа переводит в евро. Курс валюты храните в константе.  */
 
 const currentExchange = 0.85;
 const userUsdQuantity = Number(prompt('How much money do you want to exchange USD to Euro?'))
-alert(currentExchange * userUsdQuantity);
+const userEuroAmount = currentExchange * userUsdQuantity
+alert (`You will have ` + userEuroAmount + ` EURO`)
+
+/* Пользователь указывает объем флешки в Гб. Программа должна посчитать, сколько файлов размером в 820 Мб помещается на флешку.  */
 
 const gigabyte = 1024;
 const file = 820;
 const userCard = Number(prompt('How many gigabytes contain your card?'))
-alert(userCard * gigabyte / file);
+const userFilesQuantity = userCard * gigabyte / file
+alert (`Your card storage can contain ` + userFilesQuantity + ` files per 820 Mb`)
+
+/* Пользователь вводит сумму денег в кошельке и цену одной шоколадки. 
+Программа выводит, сколько шоколадок может купить пользователь, и сколько сдачи у него останется.  */
 
 const userMoney = Number(prompt('How much money do you want to spent on chokolate bars?'))
 const barPrice = Number(prompt('How much money does chokolate bar costs?'))
-alert(userMoney / barPrice)
-alert(userMoney % barPrice);
+const userChocolate = userMoney / barPrice
+const oddMoney = userMoney % barPrice
+alert (`You can buy ` + userChocolate + ` chocolate bars and your odd money is ` + oddMoney)
+
+/* Запросите у пользователя трехзначное число и выведите его задом наперед. 
+Для решения задачи вам понадобится оператор % (остаток от деления).  */
 
 const userNumber = Number(prompt('Put any number'));
 var x = userNumber;
@@ -40,11 +70,83 @@ var revers = 0;
     revers = revers * 10 + y;
     x = parseInt(x / 10);
   }
-alert (revers)
+const numberContrary = revers
+alert (`Your number on the contrary is ` + numberContrary)
 
-const userCash = Number(prompt('How much money do you want to put on deposit?'));
+/* Пользователь вводит сумму вклада в банк на 2 месяца, с процентной ставкой депозита 5% годовых. 
+Вывести сумму начисленных процентов. */
+
+const userCash = Number(prompt('How much money do you want to put on deposit?'))
 const bankYearPercent = 5
 const monthsPerYear = 12
 const twoMonthPercent = bankYearPercent / monthsPerYear * 2
 const userSummary = userCash + twoMonthPercent
-alert(userCash * twoMonthPercent / 100);
+const userEarning = userCash * twoMonthPercent / 100
+alert (`You will earn ` + userEarning + ` for 2 months`)
+
+
+/* LESSON JS-2: Типи даних та оператори
+Запросить у пользователя его возраст и определить, кем он является: 
+ребенком (0–2), подростком (12–18), взрослым (18_60) или пенсионером (60– ...). */
+
+const userAge2 = Number(prompt('How old are you?'))
+switch (true) {
+  case userAge2 < 2:
+  alert ('You are a baby');  
+    break;
+    case userAge2 > 2 && userAge2 < 12:
+      alert ('You are a children');  
+      break;
+      case userAge2 > 12 && userAge2 < 18:
+        alert ('You are a teenager');  
+        break;
+        case userAge2 > 18 && userAge2 < 60:
+        alert ('You are adult');  
+        break;
+        case userAge2 >  60:
+        alert ('You are old');  
+        break;
+  default:
+    break;
+}
+
+/* Запросить у пользователя число от 0 до 9 и вывести ему спецсимвол, который расположен на этой клавише (1–!, 2–@, 3–# и т. д). */
+
+const userSymbol = Number(prompt('Put any number from 0 to 9'))
+switch (userSymbol) {
+  case 0:
+  alert ('Your symbol is )');  
+    break;
+    case 1:
+  alert ('Your symbol is !');  
+    break;
+    case 2:
+  alert ('Your symbol is @');  
+    break;
+    case 3:
+  alert ('Your symbol is #');  
+    break;
+    case 4:
+  alert ('Your symbol is $');  
+    break;
+    case 5:
+  alert ('Your symbol is %');  
+    break;
+    case 6:
+  alert ('Your symbol is ^');  
+    break;
+    case 7:
+  alert ('Your symbol is &');  
+    break;
+    case 8:
+  alert ('Your symbol is *');  
+    break;
+    case 9:
+  alert ('Your symbol is (');  
+    break;
+  default:
+    break;
+}
+
+
+/* Запросить у пользователя трехзначное число и проверить, есть ли в нем одинаковые цифры. */
