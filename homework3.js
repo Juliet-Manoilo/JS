b43f487081e5e3b0d2766a7604e8e9778dd33dd2
juliett manoilo <mnl.juliett@gmail.com>
2020-11-15 22:06:38 +0200

    :bookmark: homework 3 finished

--- homework3.js
+++ homework3.js
@@ -112,7 +112,66 @@ document.getElementById('task26').onclick = function func() {
 /* 7. Запросить у пользователя число и на сколько цифр его сдвинуть. 
 Сдвинуть цифры числа и вывести результат (если число 123456 сдвинуть на 2 цифры, то получится 345612).*/
 document.getElementById('task27').onclick = function func() {
-
-
+    let userNumber6 = String(prompt('Put any number'));
+    let index = Number(prompt('How many digits you want to shift your number on?'));
+    alert(userNumber6.slice(index) + userNumber6.slice(0, index))
 }
+
+/* 8. Зациклить вывод дней недели таким образом: «День недели. 
+Хотите увидеть следующий день?» и так до тех пор, пока пользователь нажимает OK. */
+document.getElementById('task28').onclick = function func() {
+    const dayOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let currentDay = 0;
+    
+    while (confirm(`Today is ${dayOfWeek[currentDay]}. Do you want to know what is the next day?`)) {
+      currentDay = (currentDay + 1) % dayOfWeek.length;
+    }
+}
+/* 9. Вывести таблицу умножения для всех чисел от 2 до 9. 
+Каждое число необходимо умножить на числа от 1 до 10.*/
+document.getElementById('task29').onclick = function func() {
+    let i; 
+    let j;
+document.write("<table border=\"1\" cellspacing=\"0\" cellpadding=\"2\" align=\"center\">")
+for (i = 2; i <= 9; i++)
+{document.write("<tr>");
+    for (j = 2; j < 9; j++)
+    {document.write("<td>" + j + "*" + i + "=" + (i * j) + "</td>")}
+document.write("</tr>")
+}
+document.write("</table>")
+}
+
+/* 10. Игра «Угадай число». 
+Предложить пользователю загадать число от 0 до 100 и отгадать его следующим способом: каждую итерацию цикла делите диапазон чисел пополам, записываете результат в N и спрашиваете у пользователя «Ваше число > N, < N или == N?».
+В зависимости от того, что указал пользователь, уменьшаете диапазон. 
+Начальный диапазон от 0 до 100, поделили пополам и получили 50. 
+Если пользователь указал, что его число > 50, то изменили диапазон на от 51 до 100. 
+И так до тех пор, пока пользователь не выберет == N. */
+document.getElementById('task30').onclick = function func() {
+    alert(`Guess a number from 0 to 100`);
+    let i = 0;
+    let j = 100;
+    let userNumber7;
+    
+    while (userNumber7!=2) {
+        let N = Math.round((i + j) / 2);
+        userNumber7 = +prompt(`Put 1, if your number smaller then ${N}.
+Put 2, if your number equal to ${N}.
+Put 3, if your number bigger then ${N}.`);
+        switch (userNumber7) {
+            case 1:
+                j = --N;
+                break;
+            case 2:
+                alert(`I win, game is over)`);
+                break;
+            case 3:
+                i = ++N;
+                break;
+        }
+    }
+      
+}
+
 }
