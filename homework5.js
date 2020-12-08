82ae44011cd1c698746dfd30888c5e60c5d7684d
juliett manoilo <mnl.juliett@gmail.com>
2020-12-09 01:26:33 +0200

    :bookmark: homework for objects is done

--- homework5.js
+++ homework5.js
@@ -29,7 +29,7 @@ function calcTimeForDistance(automobile, distance = 1000) {
 
 
 autoInfo(automobile);
-const distance = prompt(`Enter distance`);
+const distance = Number(prompt(`Enter distance`));
 const timeCalc = calcTimeForDistance(automobile, distance);
 alert(`You need ${timeCalc} hours for getting your destination`)
 }
@@ -89,4 +89,48 @@ document.getElementById('task42').onclick = function func() {
     converting(firstNumber);
     converting(secondNumber);
     alert(`Look the console for checking`);
-}}
+}
+/* 3. Создать объект, описывающий время (часы, минуты, секунды), и
+    следующие функции для работы с этим объектом: 
+    Функция вывода времени на экран; 
+    Функция изменения времени на переданное количество секунд;
+    Функция изменения времени на переданное количество минут; 
+    Функция изменения времени на переданное количество часов. 
+    Учтите, что в последних 3-х функциях, при изменении одной части времени, может
+    измениться и другая. Например, если ко времени «20:30:45» добавить
+    30 секунд, то должно получиться «20:31:15», а не «20:30:75». */
+    document.getElementById('task43').onclick = function func() {
+        let time = new Date();
+        time.setHours(0);
+        
+        function display() {
+            let hours = time.getHours();
+            let minutes = time.getMinutes();
+            let seconds = time.getSeconds();
+        alert(`The time is ${hours}:${minutes}:${seconds} now`);
+
+        }
+
+        function addSeconds(s) {
+            time.setSeconds(time.getSeconds() + s);
+            alert(time.getHours()+":"+time.getMinutes()+":"+time.getSeconds());
+        }
+
+        function addMinutes(m) {
+            time.setMinutes(time.getMinutes() + m);
+            alert(time.getHours()+":"+time.getMinutes()+":"+time.getSeconds());
+        }
+
+        function addHours(h) {
+            time.setHours(time.getHours() + h);
+            alert(time.getHours()+":"+time.getMinutes()+":"+time.getSeconds());
+        }
+
+    display();
+    const userSeconds = Number(prompt('How many seconds do you want add to this time?'));
+    addSeconds(userSeconds);
+    const userMinutes = Number(prompt('How many minutes do you want add to this time?'));
+    addMinutes(userMinutes);
+    const userHours = Number(prompt('How many hours do you want add to this time?'));
+    addHours(userHours);
+    }}
